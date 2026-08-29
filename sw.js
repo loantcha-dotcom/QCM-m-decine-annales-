8b174d0356e7c9d1fabf113fd8a74caf1756de37
loantcha-dotcom <loan.tcha@gmail.com>
2026-08-28 17:49:45 -1000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = 'qcm-medecine-v16';
+const ASSETS = [
+  './','./index.html','./manifest.webmanifest',
+  './icon-192.png','./icon-512.png','./icon-512-maskable.png','./apple-touch-icon.png'
+];
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys()
+    .then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+    .then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', (e) => {
+  const req = e.request;
+  if (req.method !== 'GET') return;
+  e.respondWith(caches.match(req).then((cached) => cached || fetch(req).then((res) => {
+    if (res && res.status === 200 && new URL(req.url).origin === location.origin) {
+      const copy = res.clone(); caches.open(CACHE).then((c) => c.put(req, copy));
+    }
+    return res;
+  }).catch(() => caches.match('./index.html'))));
+});
